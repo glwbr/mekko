741262ae0c47f4d16a491a54bd81c9553af55dfb
Glauber Santana <glauber.silva14@gmail.com>
2025-04-19 14:59:56 -0300

    feat(nix): add build steps for juice Go module

--- flake.nix
+++ flake.nix
@@ -6,36 +6,64 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       systems = [
         "aarch64-linux"
         "x86_64-linux"
       ];
 
-      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f pkgsFor.${system});
-      pkgsFor = nixpkgs.lib.genAttrs systems (
-        system:
-        import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }
-      );
+      forEachSystem = nixpkgs.lib.genAttrs systems;
     in
     {
-      devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-            golangci-lint
-            treefmt
-            nixfmt-rfc-style
-          ];
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "juice";
+            version = "alpha";
+            src = ./.;
+            vendorHash = "sha256-PmOUK4yXy8J18YNsChxZ5xzUEsgZL6LDMumA3tGQzNE=";
 
-          shellHook = ''
-            echo "A juicy environment is ready!"
-          '';
+            meta = with pkgs.lib; {
+              description = "Juice - Brazilian NFC-e Invoice Extraction Library";
+              homepage = "https://github.com/glwbr/juice";
+              license = licenses.mit;
+              mainProgram = "juice";
+            };
+          };
+        }
+      );
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              golangci-lint
+              treefmt
+              nixfmt-rfc-style
+            ];
+            shellHook = ''echo "A juicy environment is ready!"'';
+          };
+        }
+      );
+
+      apps = forEachSystem (system: {
+        default = {
+          type = "app";
+          program = "${nixpkgs.legacyPackages.${system}.lib.getExe self.packages.${system}.default}";
         };
       });
+
+      defaultPackage = forEachSystem (system: self.packages.${system}.default);
+      defaultApp = forEachSystem (system: self.apps.${system}.default);
     };
 }
